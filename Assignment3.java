0dfcc45bf32746f451f841e31dfa1204e32b27da
RylanMcA <55506412+RylanMcA@users.noreply.github.com>
2019-09-20 15:09:30 -0500

    Skeleton work
    Added in the basic I/O for the main file.

--- Assignment3.java
+++ Assignment3.java
@@ -3,10 +3,19 @@ import javax.swing.JOptionPane;
 public class Assignment3{
     public static void main(String[] args){
         //Collect Car info
-        
+        String desc = JOptionPane.showInputDialog("What is the description of this car?");
+        String maxFuel = JOptionPane.showInputDialog("What is the max fuel capacity of the car?");
+        String engineDesc = JOptionPane.showInputDialog("What is the description of the engine?");
+        String mpg = JOptionPane.showInputDialog("What is the miles per gallon on the engine?");
+        String maxSpeed = JOptionPane.showInputDialog("What is the max speed of the car?");
 
-        //Create auto
-        //Auto car = new Auto();
+        //Create the engine copy
+        //Make mpg/max speed into ints.
+        Engine eng = new Engine(engineDesc, mpg, maxSpeed);
+
+        //Create auto copy
+        Auto car = new Auto(desc, maxFuel, eng);
+
         DrivePanel panel = new DrivePanel();
         JFrame app = new JFrame();
 
