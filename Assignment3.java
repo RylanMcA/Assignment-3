2d59d25fe5f264104b7e94d7573e97a5cde8386c
RylanMcA <55506412+RylanMcA@users.noreply.github.com>
2019-09-20 13:54:22 -0500

    Update Assignment3.java

--- Assignment3.java
+++ Assignment3.java
@@ -1,9 +1,9 @@
-import java.swing.JPanel;
+import javax.swing.JPanel;
 import java.awt.Graphics;
+import javax.swing.JOptionPane;
 
-public class Assignment3{
+public class Assignment3 extends JPanel {
     public static void main(String[] args){
-
 
     }
 
