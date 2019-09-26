5c9acf0d7b5bcfba340161c0e7da45a22c036912
RylanMcA <55506412+RylanMcA@users.noreply.github.com>
2019-09-26 15:46:04 -0500

    GUI Working, Adding filling up
    Lots of work done. Got the GUI working and the car actually goes.

--- Assignment3.java
+++ Assignment3.java
@@ -18,19 +18,20 @@ public class Assignment3{
 
         //Create the objects needed.
         Engine eng = new Engine(engineDesc, iMpg, iMaxSpeed);
-        Auto car = new Auto(desc, iMaxFuel, eng);        
+        Auto car = new Auto(desc, iMaxFuel, eng);  
+        car.fillUp();      
 
         //2D array to store n legs of the trip with the distance, xRatio(s), and yRatio(s)
-        String n = JOptionPane.showInputDialog("How many legs are in your trip?");
+        String n = JOptionPane.showInputDialog(car.getDescription()+"\nHow many legs are in your trip?");
         int legs = Integer.parseInt(n);
         int[] userDist = new int[legs];
         double[][] userRatio = new double[legs][2]; 
 
         //Gathers the distance and the x/y ratio for each leg of the trip
         for(int x=0;x<legs;x++){
-            String d = JOptionPane.showInputDialog("What distance do you travel in leg: "+(x+1)+"?");
-            String xr = JOptionPane.showInputDialog("What is the x ratio of leg: "+(x+1)+"?");
-            String yr = JOptionPane.showInputDialog("What is the y ratio of leg: "+(x+1)+"?");
+            String d = JOptionPane.showInputDialog(car.getDescription()+"\nWhat distance do you travel in leg: "+(x+1)+"?");
+            String xr = JOptionPane.showInputDialog(car.getDescription()+"\nWhat is the x ratio of leg: "+(x+1)+"?");
+            String yr = JOptionPane.showInputDialog(car.getDescription()+"\nWhat is the y ratio of leg: "+(x+1)+"?");
 
             userDist[x] = Integer.parseInt(d);
             userRatio[x][0] = Double.parseDouble(xr);
--- DrivePanel.java
+++ DrivePanel.java
@@ -2,36 +2,29 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 
 public class DrivePanel extends JPanel{
+    private static final long serialVersionUID = 1L;
     private int[][] coords;
-    private double[] dist;
-
     DrivePanel(int[] uDist, double[][] uRatio, Auto car){
-        dist = new double[uDist.length];
         coords = new int[uRatio.length][2];
-        
         for(int i=0;i<uRatio.length;i++){
-            dist[i] = car.drive(uDist[i],uRatio[i][0],uRatio[i][1]);
+            car.drive(uDist[i],uRatio[i][0],uRatio[i][1]);
             coords[i][0] = car.getX();
             coords[i][1] = car.getY();
+            
         }
     }
-
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        
-        int height = getHeight();   //y
-        int width = getWidth();     //x
-        
+
+        //0,0 to first point. Draw line and string.
+        g.drawLine(0,getHeight(),coords[0][0],getHeight()-coords[0][1]);
+        g.drawString("("+coords[0][0]+","+coords[0][1]+")", coords[0][0]+10, getHeight()-coords[0][1]);
+
+        //0 to array length-1 since i have to use the i and i+1
         for(int i=0;i<coords.length-1;i++){
-            
-
+            g.drawLine(coords[i][0],getHeight()-coords[i][1],coords[i+1][0],getHeight()-coords[i+1][1]);
+            g.drawString("("+coords[i+1][0]+","+coords[i+1][1]+")", coords[i+1][0]+10, getHeight()-coords[i+1][1]);
         }
-
-        g.drawLine(0,height/2,width,height/2);
-        g.drawLine(width/2,0,width/2,height);
-
-
     }
-
 }
